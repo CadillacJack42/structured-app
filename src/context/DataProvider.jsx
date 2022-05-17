9823d2ce37a81f4137e048be956a7fb100771000
James Keicher <jamesdkeicher@gmail.com>
2022-05-17 16:29:10 -0700

    feat: fleshed out DataProvider with methods

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -1,7 +1,36 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
+
+let initialState = [
+  {
+    id: '12345678-1234-1234-a234',
+    timestamp: Date.now(),
+    sender: 'Cadillac Jack',
+    message: 'Check out this cool chat app!',
+  },
+];
 
 const TextContext = createContext(null);
 
 export const DataProvider = ({ children }) => {
-  return <TextContext.Provider>{children}</TextContext.Provider>;
+  const [chats, dispatch] = useReducer(chatReducer, initialState);
+
+  const handleAdd = (message) => {
+    dispatch({ type: 'ADD', payload: { message } });
+  };
+
+  const handleEdit = (message) => {
+    dispatch({ type: 'EDIT', payload: { message } });
+  };
+
+  const handleDelete = (id) => {
+    dispatch({ type: 'DELETE', payload: { id } });
+  };
+
+  return (
+    <TextContext.Provider
+      value={{ chats, handleAdd, handleEdit, handleDelete }}
+    >
+      {children}
+    </TextContext.Provider>
+  );
 };
