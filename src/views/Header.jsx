a4d9d142d8a193fa02d348cd57ad0f68be333d0f
James Keicher <jamesdkeicher@gmail.com>
2022-05-17 16:58:09 -0700

    feat: added date to header

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from 'react';
+
 export const Header = () => {
-  return <h1>Welcome to Cadillac Jacks Chat App</h1>;
+  const [date, setDate] = useState(Date.now());
+
+  useEffect(() => {
+    const currentTime = new Date(date);
+    setDate(currentTime.toDateString());
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to Cadillac Jacks Chat App</h1>
+      <h4>{date}</h4>
+    </>
+  );
 };
