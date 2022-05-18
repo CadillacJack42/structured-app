5b9b644677f42fc1b1036621337165741c319879
James Keicher <jamesdkeicher@gmail.com>
2022-05-18 09:39:25 -0700

    refactor: Loading component changed to context. useLoading verifies LoadContext is wrapped in LoadProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { DataProvider } from './context/DataProvider';
+import { LoadingProvider } from './context/Loading';
 import { Header } from './views/Header';
 import { Chat } from './views/Chat';
 
 export default function App() {
   return (
     <>
-      <DataProvider>
-        <Header />
-        <Chat />
-      </DataProvider>
+      <LoadingProvider>
+        <DataProvider>
+          <Header />
+          <Chat />
+        </DataProvider>
+      </LoadingProvider>
     </>
   );
 }
--- src/hooks/Loading.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export const Loading = () => {
-  const [loading, setLoading] = useState(true);
-
-  return { loading, setLoading };
-};
